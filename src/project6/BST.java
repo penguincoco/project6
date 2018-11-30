b860bf40769625ceeaeeda356b6e88f631046d77
Samantha Chuang <ssc538@nyu.edu>
2018-11-30 13:01:16 -0500

    Ngl, I don't really get what I'm doing :/ But... - started trying to implement the (pre)order traversal iterator - .contains() method implemented

--- src/project6/BST.java
+++ src/project6/BST.java
@@ -2,10 +2,13 @@ package project6;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Stack;
 
+import project6.LinkedList.MyIterator;
 import project6.LinkedList.Node;
 
-public class BST<E> implements Collection<E>, Iterable<E> {
+public class BST< E extends Comparable<E>> implements Collection<E>, Iterable<E> {
+//public class BST<E> implements Comparable<E>, Collection<E>, Iterable<E> {
 	private int size;
 	private BSTNode<E> root; 
 	
@@ -30,6 +33,55 @@ public class BST<E> implements Collection<E>, Iterable<E> {
 		public int compareTo(BSTNode<E> arg0) {
 			return data.compareTo(arg0.data);
 		}
+		
+		public E getData() {
+			return this.data;
+		}
+	}
+	
+	//private, internal Iterator class
+	//preorder traversal: Root first, left subtree, right subtree
+	class BSTIterator<E extends Comparable<E>> implements Iterator<E> {
+		BSTNode<E> pointer;
+		Stack<BSTNode<E>> iteratorStack; 
+		
+		//what the fuck why does this work if I give it a parameter
+		public BSTIterator(BSTNode<E> root) {
+			iteratorStack = new Stack<>();
+			this.pointer = root; 
+			
+			if (this.pointer != null) {
+				iteratorStack.push(this.pointer);
+			}
+		}
+		
+		@Override
+		public boolean hasNext() {
+			if (pointer.left != null) {
+				iteratorStack.push(pointer.right); 
+				pointer = pointer.left;
+				return true; 
+			}
+			return false;
+		}
+
+		@Override
+		public E next() {
+			if (!hasNext()) {
+				return null; 
+			}
+			
+			E currentData = pointer.getData();
+			
+			if (pointer.left != null) {
+				pointer = pointer.left; 
+			}
+			else {
+				pointer = iteratorStack.pop();
+			}
+			
+			return currentData; 
+		}
 	}
 	
 	/**
@@ -41,9 +93,14 @@ public class BST<E> implements Collection<E>, Iterable<E> {
 		BSTNode<E> current = root;
 		
 		while (current != null) {
+			//if the data in the current node matches the parameter value, return the reference
 			if (current.data.equals(value)) {
 				return current.data;
 			}
+			
+			//else, keep checking values down the tree
+			//if compareTo returns a negative number, it means the param value is smaller than the current node so go right
+			//if compareTo returns a positive number, it means the param value is larger than the current node so go left
 			if (current.data.compareTo(value) < 0) {
 				current = current.right;
 			}
@@ -52,26 +109,28 @@ public class BST<E> implements Collection<E>, Iterable<E> {
 			}
 		}
 		
+		//if current eventually points to null, the value was not found. Return null. 
 		return null;
 	}
-	
-	
 	
 	/**
 	 * 
 	 * @param none
 	 * @return String - a string representation of this collection
 	 */
-//	public String toString() {
-//	}
+	public String toString() {
+		
+		return ""; 
+	}
 	
 //	public String toStringTreeFormat() {
 //		
 //	}
 	
+	
 	@Override
 	public boolean add(E arg0) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
@@ -79,6 +138,7 @@ public class BST<E> implements Collection<E>, Iterable<E> {
 	public boolean addAll(Collection<? extends E> arg0) {
 		// TODO Auto-generated method stub
 		return false;
+		
 	}
 
 	@Override
@@ -89,7 +149,22 @@ public class BST<E> implements Collection<E>, Iterable<E> {
 
 	@Override
 	public boolean contains(Object arg0) {
-		// TODO Auto-generated method stub
+		BSTNode<E> current = root; 
+		
+		
+		
+//		MyIterator containsIterator = new MyIterator();
+//		boolean isFound = false; 
+//		
+//		//use iterator to iterate through the list and search for the element
+//		while (containsIterator.hasNext()) 
+//		{
+//			if (containsIterator.next().equals(arg0)) 
+//			{
+//				isFound = true; 
+//			}
+//		}
+		
 		return false;
 	}
 
@@ -101,17 +176,35 @@ public class BST<E> implements Collection<E>, Iterable<E> {
 	
 	@Override
 	public boolean equals(Object o) {
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * 
+	 * @param none
+	 * @return String - a string representation of this collection
+	 */
 	@Override
-	public Iterator<E> iterator() {
+	public boolean isEmpty() {
+		if (root == null) {
+			return true;
+		}
+		else {
+			return false; 
+		}
+	}
+
+	@Override
+	public BSTIterator<E> iterator() {
 		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public Iterator<E> preorderIterator() {
+		return null;
+	}
+	
+	public Iterator<E> postorderIterator() {
 		return null;
 	}
 
@@ -150,5 +243,4 @@ public class BST<E> implements Collection<E>, Iterable<E> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
